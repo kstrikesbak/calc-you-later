a4009c78704e4161b7a914e67f9bc75fa47c17f5
Kejal Bhuva <Kejal.Bhuva@codeimmersives.com>
2019-10-07 14:15:24 -0400

    assignment complete

--- calculate.js
+++ calculate.js
@@ -1,6 +1,36 @@
-function calculate() {
-  
-}
 
+function calculate(num1,num2,operation) {
+
+    const actualNum1 = Number(num1)
+    const actualNum2 = Number(num2)
+
+    if (operation==='+') {
+    return actualNum1 + actualNum2;
+    } else if (operation==='-' ) {
+    return actualNum1 - actualNum2;
+    } else if (operation==='x' ) {
+    return actualNum1 * actualNum2;
+    } else if (operation==='/') {
+    return actualNum1 / actualNum2;
+    } else if (operation==='/') {
+    return actualNum1 / actualNum2;
+    } else if (operation==='%') {
+    return actualNum1 % actualNum2;
+    } else if (operation==='plus') {
+    return actualNum1 + actualNum2;
+    } else if (operation==='minus') {
+    return actualNum1 - actualNum2;
+    } else if (operation==='times') {
+    return actualNum1 * actualNum2;
+    } else if (operation==='X') {
+    return actualNum1 * actualNum2;
+    } else if (operation==='modulus') {
+    return actualNum1 % actualNum2;
+    } else if (operation==='mod') {
+    return actualNum1 % actualNum2;}
+    else {
+    return "Sorry, that's not a mathematical operation!"
+    }
+}
 
 module.exports = calculate;
